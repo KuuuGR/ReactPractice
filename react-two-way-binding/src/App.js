5daabfe9122a20769ebe4910c70307cedd58bfe3
Grzegorz Kulesza <kuleszagr@gmail.com>
2021-06-06 12:57:31 +0200

    button click to change state

--- react-two-way-binding/src/App.js
+++ react-two-way-binding/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handler = () => setCounter(prevValue => prevValue + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Licznik</div>
+      <button onClick={handler}>Change state</button>
+      <div >
+        <h1>
+          <p1>{counter}</p1>
+        </h1>
+
+      </div>
     </div>
   );
 }
